d1fff944b33e11d378ed805bcc54eb00d50884e9
muha <1student.muhammed1@gmail.com>
2024-05-08 17:38:36 +0300

    fix: update default date value to current timestamp

--- models/Post.js
+++ models/Post.js
@@ -6,7 +6,7 @@ const postSchema = new Schema({
     title: { type: String, required: true },
     content: { type: String, required: true },
     comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
-    date: { type: Date, default: Date.now },
+    date: { type: Date, default: Date.now()},
     author: { type: String },
     slug: {
         type: String,
